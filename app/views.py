c71199f015d0f430ca4fb1d44745859ef0ec08d8
flyingrobin <wanglirobin@gmail.com>
2016-02-08 11:07:38 -0700

    bkgd

--- app/views.py
+++ app/views.py
@@ -69,6 +69,9 @@ def synergize_output():
 def contact():
     return render_template("contact.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 @app.route('/error')
 def error_output():
